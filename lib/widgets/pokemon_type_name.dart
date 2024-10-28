a737f055a1b78541954ee545db6b3fee7e4a6b82
Actinium825 <cruz.d.ace@proton.me>
2024-10-28 15:27:15 +0800

    feat: pokemon card

--- lib/widgets/pokemon_type_name.dart
+++ lib/widgets/pokemon_type_name.dart
@@ -1,6 +1,5 @@
 import 'package:dartx/dartx.dart';
 import 'package:flutter/material.dart';
-import 'package:pokedex_flutter_riverpod/classes/pokemon_color_picker.dart';
 import 'package:pokedex_flutter_riverpod/utils/const.dart';
 import 'package:pokedex_flutter_riverpod/utils/extension.dart';
 
