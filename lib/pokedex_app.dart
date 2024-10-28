3a0b293845ef0121d2d489b1d0e718f6ec7e1a61
Actinium825 <cruz.d.ace@proton.me>
2024-10-28 12:48:28 +0800

    feat: change theme
    - add riverpod packages, shared preferences, and build runner
    - create providers for selected theme and shared preferences
    - create build yaml file with riverpod generator entry
    - ignore generated files in git ignore and analysis options
    - use custom lint plugin in analysis options
    - update ndk version in app build gradle
    - add generate step in review workflow
    - switch pokemon list page to consumer stateful widget
    - add on seelct theme function in pokemon list page
    - remove build context parameter in pokemon list page functions
    - wrap pokedex app with provider scope in main
    - get and override shared preferences in main
    - set theme mode in pokedex app
    - add string for theme shared prefes key

--- lib/pokedex_app.dart
+++ lib/pokedex_app.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokedex_flutter_riverpod/providers/selected_theme_provider.dart';
 import 'package:pokedex_flutter_riverpod/utils/router.dart';
 import 'package:pokedex_flutter_riverpod/utils/themes.dart';
 
-class PokedexApp extends StatelessWidget {
+class PokedexApp extends ConsumerWidget {
   const PokedexApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      themeMode: ref.watch(selectedThemeProvider),
       theme: PokedexTheme.themeRegular,
       darkTheme: PokedexTheme.themeDark,
       routeInformationParser: router.routeInformationParser,
